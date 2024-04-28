ba16833bb00c5bbc135bac42cdd5374c8e4a92f1
Yegor Bugayenko <yegor256@gmail.com>
2024-04-28 09:00:22 +0300

    year up

--- lib/syncem.rb
+++ lib/syncem.rb
@@ -2,7 +2,7 @@
 
 # (The MIT License)
 #
-# Copyright (c) 2018-2023 Yegor Bugayenko
+# Copyright (c) 2018-2024 Yegor Bugayenko
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
@@ -29,7 +29,7 @@
 # {README}[https://github.com/yegor256/syncem/blob/master/README.md] file.
 #
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2018-2023 Yegor Bugayenko
+# Copyright:: Copyright (c) 2018-2024 Yegor Bugayenko
 # License:: MIT
 class SyncEm
   undef_method :send
