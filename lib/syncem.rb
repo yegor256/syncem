cb4b6a7d8336f49297cfb8fa9401b24893d1ee33
Yegor Bugayenko <yegor256@gmail.com>
2026-01-01 13:11:07 +0300

    year up to 2026

--- lib/syncem.rb
+++ lib/syncem.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# SPDX-FileCopyrightText: Copyright (c) 2018-2025 Yegor Bugayenko
+# SPDX-FileCopyrightText: Copyright (c) 2018-2026 Yegor Bugayenko
 # SPDX-License-Identifier: MIT
 
 # SyncEm is a simple decorator of an existing object that makes all its
@@ -10,7 +10,7 @@
 # {README}[https://github.com/yegor256/syncem/blob/master/README.md] file.
 #
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2018-2025 Yegor Bugayenko
+# Copyright:: Copyright (c) 2018-2026 Yegor Bugayenko
 # License:: MIT
 class SyncEm
   undef_method :send
